91cc1d35a215db949155e6a7729ed8c24bd2ad18
Brian <brianforan95@gmail.com>
2018-05-27 19:21:00 -0400

    minor updates

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
 // bot stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
+const fs = require('fs');
+
+// Read commands
+const directory = './commands'
+const commands = {}
+fs.readdirSync (directory)
+  .filter (x => !fs.lstatSync (`${directory}/${x}`).isDirectory ())
+  .forEach(x => commands[x.split('.')[0]] = require (`${directory}/${x}`))
+
 
 // destructures for app
 const { botToken, game, prefix, db} = require('./config.json');
 const { log } = console;
 
-const SELF = true;
-
 client.on('ready', () => {
-    log('Ready');
     client.user.setGame(game);
-
 });
 
 client.on('message', msg => {
-    if(msg.content.toLowerCase() === 'good bot') {
-        msg.channel.send(':drooling_face:');
+    if(msg.content.toLowerCase() === 'hello') {
+        msg.channel.send('hello :D');
         return;
     }
 
-    if(msg.content.indexOf(prefix) !== 0) return; //require !
+    if(msg.content.indexOf(prefix) !== 0) return; //require prefix
 
     //Split args
     const args = msg.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    const dbCommands = ['tag'];
-
     try {
-        if(dbCommands.includes(command)) {
-            client.knex = require('knex')({
-                client: 'mysql2',
-                connection: {
-                    host: db.server,
-                    user: db.user,
-                    password: db.pass,
-                    database: db.name,
-                    port: db.port
-                }
-            });
-        }
-        let commandFile = require(`./commands/${command}.js`);
-        commandFile.run(client, msg, args);
+        commands[command].run(client, msg, args);
         console.log(`Command ${command} has been run`);
     } catch (err) {
         console.error(err);
--- commands/calc.js
+++
@@ -1,5 +0,0 @@
-exports.run = (client, msg, args) => {
-    const str = args.toString();
-    const ans = eval(`${str}`);
-    msg.channel.send(`\`${str}\` = ${ans}`);
-}
